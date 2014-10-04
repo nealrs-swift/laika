62221bf86e9561d415801378e4031ef9aaee16fe
Neal Shyam <neal@audioshocker.com>
2014-10-04 11:13:34 -0400

    added real dog years calculation

--- Laika/ViewController.swift
+++ Laika/ViewController.swift
@@ -21,8 +21,23 @@ class ViewController: UIViewController {
         
         ageLabel.text = "\(age)" + " dog years"
         ageLabel.textColor = UIColor.blueColor()
+    }
+    
+    
+    
+    @IBAction func realYears(sender: UIButton) {
+        var age = Double((ageInput.text as NSString).doubleValue)
+        ageInput.text = ""
+        ageInput.resignFirstResponder()
         
+        if age <= 2{
+            age *= 10.5
+        } else {
+            age = (age-2)*4 + 21
+        }
         
+        ageLabel.text = "\(age)" + " dog years"
+        ageLabel.textColor = UIColor.redColor()
     }
     
     override func viewDidLoad() {
